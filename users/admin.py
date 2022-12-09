06532eb096a32f32f80fdc5aa104a72e5270567f
Bruna <bruna.dall9741@gmail.com>
2022-12-09 12:15:25 -0300

    Registrando models no Django Admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users.models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    readonly_fields = ("id",)
+
+    fieldsets = (
+        ("Credenciais", {"fields": ("username", "password")}),
+        (
+            "Personal Info",
+            {
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                )
+            },
+        ),
+        ("Permissions", {"fields": ("is_employee", "is_superuser")}),
+        ("Dates", {"fields": ("birthdate",)}),
+    )
+
+admin.site.register(User, CustomUserAdmin)
